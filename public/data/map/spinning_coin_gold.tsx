6732b3dec6d8fc35faa7bbdc0a7ececddd469c24
David Morrison <DDM2@email.sc.edu>
2023-02-01 17:56:37 -0500

    Delete public directory

--- public/data/map/spinning_coin_gold.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="spinning_coin_gold" tilewidth="32" tileheight="32" tilecount="8" columns="8">
- <image source="../img/spinning_coin_gold.png" width="256" height="32"/>
-</tileset>
