cf5054b80f1cd5e45fce659c8a7a14046c112910
David Morrison <DDM2@email.sc.edu>
2023-02-01 13:42:27 -0500

    Add files via upload

---
+++ public/data/map/streets-tilee-v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="streets-tilee-v2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../img/streets-tilee-v2.png" trans="ffffff" width="512" height="512"/>
+</tileset>
